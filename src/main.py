fecd937c5257917a8b09d9a3980608907fb1c3a5
NinjaCrusader <jeroldpemberton@gmail.com>
2025-05-07 20:49:43 -0400

    This commit is the first commit to get the project to deploy through github

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import sys
 from block_splitter import markdown_to_blocks, markdown_to_html_node
 from htmlnode import *
 from pathlib import Path
@@ -23,15 +24,17 @@ def copy_static_to_public(source_path, dest_path):
             copy_static_to_public(source_item_path, dest_item_path)
 
 
-def get_files_ready():  
-    if os.path.exists('./public'):
-        shutil.rmtree('./public')
-        print("Content in public file was deleted!")
-    os.mkdir('./public')
-    if not os.path.exists('./static'):
+def get_files_ready(script_dir):
+    docs_dir = "docs"
+    static_dir = "static"
+    if os.path.exists(docs_dir):
+        shutil.rmtree(docs_dir)
+        print("Content in docs dictionary was deleted!")
+    os.mkdir(docs_dir)
+    if not os.path.exists(static_dir):
         print("Error: Static directory not found!")
         return
-    copy_static_to_public('./static', './public')
+    copy_static_to_public(static_dir, docs_dir)
 
 
 def extract_title(markdown):
@@ -53,7 +56,7 @@ def extract_title(markdown):
 #     Write the new full HTML page to a file at dest_path. Be sure to create any necessary directories if they don't exist.
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     if not os.path.exists(from_path):
         raise FileNotFoundError(f"Path {from_path} does not exist")
@@ -70,7 +73,7 @@ def generate_page(from_path, template_path, dest_path):
     print("Extracting title...")
     title = extract_title(read_from)
     print("Putting the Title and Content in the final html file. Please wait...")
-    final_html = read_temp.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    final_html = read_temp.replace("{{ Title }}", title).replace("{{ Content }}", html_content).replace('href="/', f'href={basepath}').replace('src="/', f'src={basepath}')
     print("On to the next step")
     directory_path = os.path.dirname(dest_path)
     if not os.path.exists(directory_path):
@@ -81,7 +84,7 @@ def generate_page(from_path, template_path, dest_path):
         print(dest_path + " " + "was made")
     
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     print(f"Checking if {dir_path_content} exists...")
     if not os.path.exists(dir_path_content):
         raise FileNotFoundError(f"Path {dir_path_content} does not exist")
@@ -103,7 +106,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 print("content converted to html, now extracting title...")
                 title = extract_title(read_mark)
                 print("Putting the Title and Content in the final html file. Please wait...")
-                final_html = read_temp.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+                final_html = read_temp.replace("{{ Title }}", title).replace("{{ Content }}", html_content).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
                 relative_path = item.relative_to(content_path)
                 output_filename = relative_path.with_suffix('.html')
                 output_path = Path(dest_dir_path) / output_filename
@@ -113,7 +116,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         elif item.is_dir():
             new_dest_dir = Path(dest_dir_path) / item.name
             new_dest_dir.mkdir(parents=True, exist_ok=True)
-            generate_pages_recursive(item, template_path, new_dest_dir)
+            generate_pages_recursive(item, template_path, new_dest_dir, basepath)
 
         
     
@@ -122,8 +125,16 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
 
 def main():
-  get_files_ready()
-  generate_pages_recursive("/home/ninja/workspace/github.com/NinjaCrusader/site_generator/content/", "/home/ninja/workspace/github.com/NinjaCrusader/site_generator/template.html", "/home/ninja/workspace/github.com/NinjaCrusader/site_generator/public/")  
+  if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+  else:
+    basepath = '/'
+  script_dir = os.path.dirname(os.path.abspath(__file__))  
+  get_files_ready(script_dir)
+  content_path = "content"
+  template_path = "template.html"
+  docs_path = "docs"
+  generate_pages_recursive(content_path, template_path, docs_path, basepath)  
     
     
 
